e64835c4b7b26990aa9485401dcdded53a2ca46b
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-04-10 16:27:15 +0530

    Update confirm.blade.php

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Confirm Password') }}</div>
 
                 <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    {{ __('Please confirm your Password before continuing.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
                         @csrf
