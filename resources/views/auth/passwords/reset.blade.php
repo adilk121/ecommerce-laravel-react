dceedad5d4686bc26b50e93bbe33c916f267b911
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-04-10 16:28:19 +0530

    Update reset.blade.php

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -52,7 +52,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Reset Your Password') }}
                                 </button>
                             </div>
                         </div>
